7d41891a186116de2aff80b8f406ccbb316c4a55
dkrishnatrey <dkrishnatrey@gmail.com>
2021-08-31 00:56:21 +0530

    cart test added

---
+++ src/test/java/com/crm/qa/testcases/CartTest.java
@@ -0,0 +1,12 @@
+package com.crm.qa.testcases;
+import org.testng.annotations.Test;
+
+public class CartTest {
+	
+	@Test(priority = 1)
+	public void GitTest() {
+		System.out.println("Git branch test");
+		
+	}
+
+}
